61f77905842b0748e464aa443e46d8f1339a74fb
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-24 13:22:47 +0300

    Change tasks and rename project build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,9 @@
 
 // Build script file extensions must be .gradle.kts
 
+// Tasks block must be changed
+
 buildscript {
-    ext.kotlin_version = "1.3.41"
     repositories {
         google()
         jcenter()
@@ -22,7 +23,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.6.0-alpha05")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.41")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -36,6 +37,9 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+// Tasks must look like this
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
